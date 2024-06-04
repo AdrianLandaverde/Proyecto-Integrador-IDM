5585e2531e9f6467363eebe9bee834d4510bd3b2
AdrianLandaverde <adrianlandaverden@gmail.com>
2024-06-04 08:26:17 -0600

    :sparkles: Template clients

--- react-app/src/sections/Clients.jsx
+++ react-app/src/sections/Clients.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Clients.css';
 
 const Clients = () => {
     return (
         <div className='clients'>
-            <h1>Welcome to the Clients Page</h1>
-            <Link to="/app">Go to App</Link>
+            <iframe src='cuentas_por_tipo.html' width="50%" height="300px" style={{border: "none"}}></iframe>
+            <iframe src='cuentas_por_sexo.html' width="50%" height="300px" style={{border: "none"}}></iframe>
+            <iframe src='cuentas_por_tipo_cliente.html' width="50%" height="300px" style={{border: "none"}}></iframe>
+            <iframe src='ingresos_de_los_clientes.html' width="50%" height="300px" style={{border: "none"}}></iframe>
         </div>
     );
 }
