c8a1e7e03f724ff1d160c9eda35e58d39e854d05
AdrianLandaverde <adrianlandaverden@gmail.com>
2024-06-04 09:25:56 -0600

    :lipstick: Change colors

--- react-app/src/sections/Clients.jsx
+++ react-app/src/sections/Clients.jsx
@@ -4,10 +4,10 @@ import './Clients.css';
 const Clients = () => {
     return (
         <div className='clients'>
-            <iframe src='cuentas_por_tipo.html' width="50%" height="300px" style={{border: "none"}}></iframe>
-            <iframe src='cuentas_por_sexo.html' width="50%" height="300px" style={{border: "none"}}></iframe>
-            <iframe src='cuentas_por_tipo_cliente.html' width="50%" height="300px" style={{border: "none"}}></iframe>
-            <iframe src='ingresos_de_los_clientes.html' width="50%" height="300px" style={{border: "none"}}></iframe>
+            <iframe className='iframeStyle' src='cuentas_por_tipo.html'></iframe>
+            <iframe className='iframeStyle' src='cuentas_por_sexo.html'></iframe>
+            <iframe className='iframeStyle' src='cuentas_por_tipo_cliente.html'></iframe>
+            <iframe className='iframeStyle' src='ingresos_de_los_clientes.html'></iframe>
         </div>
     );
 }
