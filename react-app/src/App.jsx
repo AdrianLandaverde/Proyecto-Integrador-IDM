86614d844870b0232c27f5d0a9b24b788d306801
AdrianLandaverde <adrianlandaverden@gmail.com>
2024-06-05 12:02:48 -0600

    :lipstick: Improve home design

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -16,23 +16,23 @@ const App = () => {
                 <nav className='nav'>
                     <ul>
                         <li>
-                            <NavLink to="/" activeClassName='active' exact>Home</NavLink>
-                            <NavLink to="/Clients" activeClassName='active'>Clients</NavLink>
-                            <NavLink to="/Map" activeClassName='active'>Map</NavLink>
-                            <NavLink to="/Credit" activeClassName='active'>Credit</NavLink>
-                            <NavLink to="/Classification" activeClassName='active'>Classification</NavLink>
-                            <NavLink to="/Interpolation" activeClassName='active'>Interpolation</NavLink>
+                            <NavLink to="/" activeClassName='active' exact>Inicio</NavLink>
+                            <NavLink to="/Clientes" activeClassName='active'>Clientes</NavLink>
+                            <NavLink to="/Demografía" activeClassName='active'>Demografía</NavLink>
+                            <NavLink to="/Ofertas" activeClassName='active'>Ofertas</NavLink>
+                            <NavLink to="/Clasificacion" activeClassName='active'>Clasificacion</NavLink>
+                            <NavLink to="/Interpolacion" activeClassName='active'>Interpolacion</NavLink>
                         </li>
                     </ul>
                 </nav>
 
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/Clients" element={<Clients />} />
-                    <Route path="/Map" element={<Map />} />
-                    <Route path="/Credit" element={<Credit />} />
-                    <Route path="/Classification" element={<Classification />} />
-                    <Route path="/Interpolation" element={<Interpolation />} />
+                    <Route path="/Clientes" element={<Clients />} />
+                    <Route path="/Demografía" element={<Map />} />
+                    <Route path="/Ofertas" element={<Credit />} />
+                    <Route path="/Clasificacion" element={<Classification />} />
+                    <Route path="/Interpolacion" element={<Interpolation />} />
                 </Routes>
             </div>
         </Router>
--- react-app/src/sections/Home.jsx
+++ react-app/src/sections/Home.jsx
@@ -1,46 +1,78 @@
 import React from 'react';
 import './Home.css';
+import { Grid, Typography, Box } from '@material-ui/core';
+
 
 const Home = () => {
     return (
         <div className='home'>
-            <h1>Welcome to Our Project</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod ultrices massa, et pharetra justo fermentum nec.</p>
-            <img src="path_to_your_company_image.jpg" alt="Company" />
-            <h2>Our Team</h2>
+
+            <Box className="fullScreenContainer">
+                <Grid container spacing={3} className="container">
+                    <Grid item xs={12}>
+                        <h1>Análisis Bancario</h1>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <Typography variant="body1" className="description">
+                            Recientemente, una destacada empresa bancaria solicitó a DataIQ una Prueba de Concepto (POC) en Qlik Sense para evaluar diversas herramientas de BI.
+                        </Typography>
+
+                        <Typography variant="body1" className="description">
+                            Los principales aspectos a evaluar incluyen la integración de múltiples fuentes de información, un modelo de datos eficiente, la interpretación de reglas de negocio y un diseño innovador que facilite el análisis y la toma de decisiones.
+                        </Typography>
+
+                        <Typography variant="body1" className="description">
+                            La POC se centrará en analizar Ofertas/Promociones de Crédito basadas en varios criterios de elegibilidad. Para ello, se proporcionaron datos de Access y reglas de negocio específicas.
+                        </Typography>
+
+                        <Typography variant="body1" className="description">
+                            La efectividad en el uso de la herramienta será crucial para la decisión de la empresa, y se busca aprovechar tu experiencia en ingeniería de datos y matemáticas para innovar en el análisis de la información
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <Box display="flex" flexDirection="column" alignItems="center">
+                            <img className="image" src="images/tec.jpeg" alt="Company1" />
+                            <img className="image" src="https://dataiq.mx/wp-content/uploads/2022/04/logo-DataIQ.png" alt="Company2" />
+                            <img className="image" src="https://www.prisma-informatik.de/newsroom/wp-content/uploads/2019/06/Qlik-Logo_RGB-768x226.png" alt="Company3" />
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Box>
+
+            <h2>Nuestro Equipo</h2>
             <div className='team'>
                 <div className='team-member'>
-                    <img src="path_to_team_member_1_image.jpg" alt="Team member 1" />
-                    <p>Team member 1</p>
+                    <img src="images/adrian.JPG" alt="Team member 1" />
+                    <p>Adrián Landaverde Nava</p>
                 </div>
                 <div className='team-member'>
-                    <img src="path_to_team_member_2_image.jpg" alt="Team member 2" />
-                    <p>Team member 2</p>
-                </div>
-
-                <div className='team-member'>
-                    <img src="path_to_team_member_3_image.jpg" alt="Team member 3" />
-                    <p>Team member 3</p>
+                    <img src="images/yair.png" alt="Team member 2" />
+                    <p>Jesús Yair Ramirez Islas</p>
                 </div>
 
                 <div className='team-member'>
-                    <img src="path_to_team_member_4_image.jpg" alt="Team member 4" />
-                    <p>Team member 4</p>
+                    <img src="images/luis.jpeg" alt="Team member 3" />
+                    <p>Luis Gerardo Lagunes Nájera</p>
                 </div>
 
                 <div className='team-member'>
-                    <img src="path_to_team_member_5_image.jpg" alt="Team member 5" />
-                    <p>Team member 5</p>
+                    <img src="images/cristian.jpeg" alt="Team member 4" />
+                    <p>Cristian Gonzaga López</p>
                 </div>
 
                 <div className='team-member'>
-                    <img src="path_to_team_member_6_image.jpg" alt="Team member 6" />
-                    <p>Team member 6</p>
+                    <img src="images/evelyn.jpg" alt="Team member 5" />
+                    <p>Evelyn Geovanna Pérez Gómez</p>
                 </div>
-            
+
                 <div className='team-member'>
-                    <img src="path_to_team_member_7_image.jpg" alt="Team member 7" />
-                    <p>Team member 7</p>
+                    <img src="images/michael.jpg" alt="Team member 6" />
+                    <p>Michael Steven Delgado Caicedo</p>
+                </div>
+
+                <div className='team-member'>
+                    <img src="images/" alt="Team member 7" />
+                    <p>Deborah Tirado</p>
                 </div>
 
             </div>
